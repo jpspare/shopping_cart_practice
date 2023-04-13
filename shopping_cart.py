411e0c0bd9daeec8a4dfca2020e4e44ae2d48d3e
Caleb Spare <cespare@gmail.com>
2023-04-13 10:38:19 -0700

    Fix whitespace/spelling

--- shopping_cart.py
+++ shopping_cart.py
@@ -4,22 +4,22 @@ from IPython.display import clear_output
 
 def shoppingCart():
     """
-        Recieves input to either add items to a list (shopping cart), delete items from that list,
-        view the list, or tell the function to quit, at which any items in the cart will be printed
-        for viewing.
+    Receives input to either add items to a list (shopping cart), delete items from that list,
+    view the list, or tell the function to quit, at which any items in the cart will be printed
+    for viewing.
     """
-    
+
     cart = []
-    
+
     shopping = True
-    
+
     while shopping:
         # Intro message and input
         print("\nWelcome to Food and Stuff! What would you like to do today?")
         action = input("\n1. Show my cart \n2. Add to my cart \n3. Delete something from my cart \n4. Quit\n")
         clear_output()
         action_check = action.lower()
-        
+
         # Option 1, to view cart
         if action_check == '1' or action_check == 'show':
             if cart == []:
@@ -28,7 +28,7 @@ def shoppingCart():
                 print("Here is what you have in your cart:")
                 for item in cart:
                     print(item)
-        
+
         # Option 2, to add to cart
         elif action_check == '2' or action_check == 'add':
             add_item = input("What would you like to add to your cart? ")
@@ -36,7 +36,7 @@ def shoppingCart():
             add_item_pretty = add_item.title()
             cart.append(add_item.title())
             print("Great! We have added " + add_item + " to your cart!")
-            
+
         # Option 3, to delete from cart
         elif action_check == '3' or action_check == 'delete':
             delete_item = input("What would you like to remove from your cart? ")
@@ -56,7 +56,7 @@ def shoppingCart():
                 clear_output()
                 cart.remove(delete_item_pretty)
                 print("We've removed " + delete_item_pretty + " from your cart!")
-                
+
         # Option 4, to quit and print the cart
         elif action_check == '4' or action_check == 'quit':
             clear_output()
@@ -64,10 +64,10 @@ def shoppingCart():
             for index, item in enumerate(cart, start=1):
                 print(str(index) + ". " + item)
             break
-        
+
         # Alternative if input doesn't match expected values
         else:
             print("I'm sorry, I'm not quite sure what you are looking to do. Please enter one of the following: Show, Add, Delete, or Quit")
-            
+
 
 shoppingCart()
